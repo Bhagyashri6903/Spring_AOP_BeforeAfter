a19009ded177e7c7844990a8ebfb94af01e054ec
Bhagyashri <bhagyashripatil434@gmail.com>
2025-08-07 21:38:31 +0530

    Commit

--- src/main/java/com/bank/aop/Transaction.java
+++ src/main/java/com/bank/aop/Transaction.java
@@ -1,22 +1,42 @@
 package com.bank.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class Transaction {
-	
-	@Before("execution(* com.bank.entity.BankService.*(..))")
-	public void beforeTransaction() {
-		System.out.println("Transaction will begin");
-	}
-	
-	@After("execution(* com.bank.entity.BankService.*(..))")
-	public void afterTransaction() {
-		System.out.println("The transaction is completed");
-	}
 
+    
+    @Before("execution(* com.bank.entity.BankService.*(..))")
+    public void beforeTransaction() {
+        System.out.println("Transaction will begin");
+    }
+
+    
+    @After("execution(* com.bank.entity.BankService.*(..))")
+    public void afterTransaction() {
+        System.out.println("The transaction is completed");
+    }
+
+    
+    @AfterReturning(
+        pointcut = "execution(* com.bank.entity.BankService.deposit(..))",
+        returning = "amount"
+    )
+    public void afterDepositSuccess(double amount) {
+        System.out.println("Successfully deposited " + amount + "rs");
+    }
+
+
+    
+    @Around("execution(* com.bank.entity.BankService.withdraw(..))")
+    public Object aroundWithdraw(ProceedingJoinPoint projp) throws Throwable {
+        System.out.println("Checking withdrawal conditions");
+        Object result = projp.proceed();  
+        System.out.println("Withdrawal completed.");
+        return result;
+    }
 }
--- src/main/java/com/bank/entity/BankService.java
+++ src/main/java/com/bank/entity/BankService.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class BankService {
 	
-	public void withdraw(double amount) {
-		System.out.println("The amount withdraw from your account : " + amount + "rs");
-	}
-    
 	public void deposit(double amount) {
 		System.out.println(amount + "rs" +  " Deposited to your account");
+		
 	}
+	
+	public void withdraw(double amount) {
+		System.out.println("The amount withdraw from your account : " + amount + "rs");
+		
+	}
+    
 }
